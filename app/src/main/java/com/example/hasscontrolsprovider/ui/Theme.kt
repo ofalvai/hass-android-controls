97c5dc887e5a4777accc68a5454d1ce01950e808
Olivér Falvai <ofalvai@gmail.com>
2020-11-05 15:06:38 +0100

    Upgrade libs, Gradle, AGP

--- app/src/main/java/com/example/hasscontrolsprovider/ui/Theme.kt
+++ app/src/main/java/com/example/hasscontrolsprovider/ui/Theme.kt
@@ -1,12 +1,12 @@
 package com.example.hasscontrolsprovider.ui
 
-import androidx.ui.graphics.Color
-import androidx.ui.material.lightColorPalette
+import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
 
 val primaryColor = Color(0xFF03A9F4)
 val primaryVariant = Color(0xFF0288D1)
 
-val colorPalette = lightColorPalette(
+val colorPalette = lightColors(
     primary = primaryColor,
     primaryVariant = primaryVariant
 )
