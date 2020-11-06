5c8d3dcd5a9acc2a2e476966a97c494d891fb961
Olivér Falvai <ofalvai@gmail.com>
2020-11-06 14:03:29 +0100

    Compose components for light, switch, sensor

--- app/src/main/java/com/example/hasscontrolsprovider/ui/MainActivity.kt
+++ app/src/main/java/com/example/hasscontrolsprovider/ui/MainActivity.kt
@@ -2,28 +2,25 @@ package com.example.hasscontrolsprovider.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.Composable
-import androidx.compose.getValue
-import androidx.compose.setValue
-import androidx.compose.state
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.state
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.ui.core.Modifier
-import androidx.ui.core.setContent
-import androidx.ui.foundation.Text
-import androidx.ui.graphics.Color
-import androidx.ui.layout.Column
-import androidx.ui.layout.Row
-import androidx.ui.layout.padding
-import androidx.ui.livedata.observeAsState
-import androidx.ui.material.Divider
-import androidx.ui.material.MaterialTheme
-import androidx.ui.material.Slider
-import androidx.ui.material.Switch
 import androidx.ui.tooling.preview.Preview
-import androidx.ui.unit.dp
-import com.example.hasscontrolsprovider.entity.Availability
-import com.example.hasscontrolsprovider.entity.HassControl
+import com.example.hasscontrolsprovider.R
+import com.example.hasscontrolsprovider.entity.*
 import com.example.hasscontrolsprovider.mapper.PendingIntentConstants
 
 class MainActivity : AppCompatActivity() {
@@ -36,9 +33,6 @@ class MainActivity : AppCompatActivity() {
             override val name = intent.getStringExtra(PendingIntentConstants.EXTRA_NAME) ?: ""
             override val status = intent.getStringExtra(PendingIntentConstants.EXTRA_STATUS) ?: ""
         }
-
-//        val switchView = android.widget.Switch(this)
-//        setContentView(switchView)
 
         val hassControlLiveData: LiveData<HassControl> = MutableLiveData(hassControl)
 
@@ -58,22 +52,45 @@ fun Light(hassControl: LiveData<HassControl>, onToggle: (Boolean) -> Unit) {
     var brightness by state { 0f }
 
     Column(modifier = Modifier.padding(16.dp)) {
-        Text(text = controlState.value?.name ?: "", style = MaterialTheme.typography.h5)
-        Divider(color = Color.Gray)
-        Switch(
-            color = primaryColor,
-            checked = entityState,
-            onCheckedChange = {
-                entityState = it
-                onToggle(it)
-            },
-            modifier = Modifier.padding(horizontal = 32.dp, vertical = 16.dp)
+        EntityInfo(hassControl) {
+            Switch(
+                checked = entityState,
+                onCheckedChange = {
+                    entityState = it
+                    onToggle(it)
+                }
+            )
+        }
+
+        Text(
+            text = "Brightness",
+            style = MaterialTheme.typography.caption
         )
-        Slider(
-            value = brightness,
-            onValueChange = { brightness = it },
-            valueRange = 0f..100f
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(vectorResource(R.drawable.ic_brightness))
+            Slider(
+                modifier = Modifier.padding(start = 16.dp),
+                value = brightness,
+                onValueChange = { brightness = it },
+                valueRange = 0f..100f
+            )
+        }
+
+        Text(
+            text = "Color temperature",
+            style = MaterialTheme.typography.caption
         )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(vectorResource(R.drawable.ic_color_temp))
+            Slider(
+                modifier = Modifier.padding(start = 16.dp),
+                value = .0f,
+                onValueChange = {  },
+                valueRange = 0f..100f
+            )
+        }
+
+
         Divider(color = Color.Gray)
         EntityAttribute(name = "State", value = if (entityState) "ON" else "OFF")
         EntityAttribute(name = "Brightness", value = "${brightness.toInt()}%")
@@ -81,29 +98,125 @@ fun Light(hassControl: LiveData<HassControl>, onToggle: (Boolean) -> Unit) {
 }
 
 @Composable
+fun Switch(hassControl: LiveData<HassControl>, onToggle: (Boolean) -> Unit) {
+    val controlState = hassControl.observeAsState()
+    var entityState by state { false }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        EntityInfo(hassControl) {
+            Switch(
+                checked = entityState,
+                onCheckedChange = {
+                    entityState = it
+                    onToggle(it)
+                }
+            )
+        }
+
+        Divider(color = Color.Gray)
+        EntityAttribute(name = "State", value = if (entityState) "ON" else "OFF")
+    }
+}
+
+@Composable
+fun Sensor(hassControl: LiveData<HassControl>) {
+    val controlState = hassControl.observeAsState()
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        EntityInfo(hassControl) {
+            Text(controlState.value?.status ?: "")
+        }
+    }
+}
+
+@Composable
+fun EntityInfo(hassControl: LiveData<HassControl>,
+               currentStateContent: @Composable () -> Unit) {
+    val controlState = hassControl.observeAsState()
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(vectorResource(R.drawable.ic_lightbulb))
+            Column(modifier = Modifier.padding(start = 16.dp)) {
+                Text(
+                    text = controlState.value?.name ?: "",
+                    style = MaterialTheme.typography.subtitle1
+                )
+                Text(
+                    text = "23 minutes ago", // TODO
+                    style = MaterialTheme.typography.subtitle1
+                )
+            }
+        }
+        currentStateContent()
+    }
+}
+
+@Composable
 fun EntityAttribute(name: String, value: String) {
-    Row {
+    Row(horizontalArrangement = Arrangement.SpaceBetween) {
         Text(text = "$name: ", style = MaterialTheme.typography.body1)
         Text(text = value, style = MaterialTheme.typography.body1)
     }
 }
 
-@Preview(widthDp = 200, showBackground = true)
+@Preview(widthDp = 300, showBackground = true)
 @Composable
-fun PreviewEntity() {
-    val hassControl = object : HassControl {
-        override val entityId = "light.living_room"
-        override val availability = Availability.AVAILABLE
-        override val name = "Living room light"
-        override val status = "OFF"
+fun PreviewLightEntity() {
+    val light = HassLight(
+        entityId = "light.living_room",
+        availability = Availability.AVAILABLE,
+        name = "Living Room",
+        state = true,
+        status = "on",
+        features = setOf(LightFeatures.BRIGHTNESS, LightFeatures.COLOR_TEMP),
+        brightness = 50,
+        colorTemp = 0
+    )
+    HassTheme {
+        Light(MutableLiveData(light), onToggle = {})
+    }
+}
+
+@Preview(widthDp = 300, showBackground = true)
+@Composable
+fun PreviewSwitchEntity() {
+    val switch = HassSwitch(
+        entityId = "switch.zigbee2mqtt_permit_join",
+        availability = Availability.AVAILABLE,
+        name = "Zigbee2MQTT permit join",
+        status = "on",
+        enabled = false
+    )
+    HassTheme {
+        Switch(MutableLiveData(switch), onToggle = {})
+    }
+}
+
+@Preview(widthDp = 300, showBackground = true)
+@Composable
+fun PreviewSensorEntity() {
+    val sensor = object : HassControl {
+        override val entityId = "sensor.temperature"
+        override val availability =Availability.AVAILABLE
+        override val name = "Temperature"
+        override val status = "22.81 °C"
+
     }
     HassTheme {
-        Light(MutableLiveData(hassControl), onToggle = {})
+        Sensor(MutableLiveData(sensor))
     }
 }
 
 @Composable
-fun HassTheme(content: @Composable() () -> Unit) {
+fun HassTheme(content: @Composable () -> Unit) {
     MaterialTheme(colors = colorPalette) {
         content()
     }
